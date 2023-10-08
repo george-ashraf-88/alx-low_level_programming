13b3a7d37b6f77ac0dbc1a823010b16a4343c6a9
george-ashraf-88 <georgeashraf88@icloud.com>
2023-10-09 01:28:26 +0200

    task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main - prints all single digit numbers of base 10 starting from 0
  *des.
@@ -8,7 +9,7 @@ int main(void)
 {
 	int x;
 
-	for (x=0; x <=9; x++)
+	for (x=0; x < 10; x++)
 	{
 		putchar("%d", x);
 	}
