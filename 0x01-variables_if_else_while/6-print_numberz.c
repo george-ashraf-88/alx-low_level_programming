880747d32241a332d28d02eec38defcab9901db6
george-ashraf-88 <georgeashraf88@icloud.com>
2023-10-09 01:25:43 +0200

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - prints all single digit numbers of base 10 starting from 0
+ *des.
+ *Return: always (0)
+ */
+int main(void)
+{
+	int x;
+
+	for (x=0; x <=9; x++)
+	{
+		putchar("%d", x);
+	}
+	putchar("\n");
+
+	return (0);
+}
