e341cd38783b5a23cbbda632f0d640a22bdba8be
george-ashraf-88 <georgeashraf88@icloud.com>
2023-10-08 16:40:11 +0200

    task 2,3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdout.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char letter;
+	for (letter = "a" ; letter <= "z" ; letter++);
+		putchar(letter)
+	putchar("\n")
+
+	return (0)
+}
