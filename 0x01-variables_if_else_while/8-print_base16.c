bb4ee079d54c2c95a37117d0bcec3bea95d5d5f7
george-ashraf-88 <georgeashraf88@icloud.com>
2023-10-09 01:52:02 +0200

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main  Prints all the numbers of base 16 in lowercase
+ *
+ *Return:always (0)
+ **/
+int main(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x > 10; x++)
+	{
+		putchar(x + '0');
+	}
+
+	for (y = 'a'; y < 'g'; y++)
+	{
+		putchar(y);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
