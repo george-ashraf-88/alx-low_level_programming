00e689d6ae824e3213e061b889a1f279fb79a45a
george-ashraf-88 <georgeashraf88@icloud.com>
2023-10-09 02:32:51 +0200

    task100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,16 +6,16 @@
  **/
 int main(void)
 {
-	int x, y ,c;
+	int x, y;
 
 	for (x = 0; x < 10; x++)
 	{
 		for (y = x + 1; y < 10; y++)
 		{
 			putchar(x + '0');
-			putchar(y +'0');
+			putchar(y + '0');
 			
-			if ( x !=8 || y != 9)
+			if (x != 8 || y != 9)
 				putchar(',');
 				putchar(' ');
 		}
