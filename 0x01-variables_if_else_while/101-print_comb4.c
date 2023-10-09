ec060c37f0fa8c2b763817239b3bb89af3021fc3
george-ashraf-88 <georgeashraf88@icloud.com>
2023-10-09 02:47:36 +0200

    task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,9 +10,9 @@ int main(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = x+1; y < 10; y++)
+		for (y = x + 1; y < 10; y++)
 		{
-			for (z = y+1; z < 10; z++)
+			for (z = y + 1; z < 10; z++)
 			{
 				if (z > y && y > x)
 				{
