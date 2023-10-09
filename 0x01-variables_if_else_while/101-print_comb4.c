d29dda0ec0a66da16b5b5b68c5880a456f781de1
george-ashraf-88 <georgeashraf88@icloud.com>
2023-10-09 02:46:28 +0200

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ *main -prints all possible combinations of three-digit numbers
+ *
+ *Return:always (0)
+ **/
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = x+1; y < 10; y++)
+		{
+			for (z = y+1; z < 10; z++)
+			{
+				if (z > y && y > x)
+				{
+					putchar(x + '0');
+					putchar(y + '0');
+					putchar(z + '0');
+					if (x != 7 || y != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
