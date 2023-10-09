a912a266634f844b4e743b9ac89acca9f9e4dde2
george-ashraf-88 <georgeashraf88@icloud.com>
2023-10-09 02:07:06 +0200

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main -prints all possible combinations of single-digit numbers
+ *
+ *Return:always (0)
+ **/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
