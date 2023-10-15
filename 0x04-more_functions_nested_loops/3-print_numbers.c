4a1a3ef6483d968ad2d72dbc662f4c5ce70dfc10
george-ashraf-88 <georgeashraf88@icloud.com>
2023-10-15 10:54:15 -0700

    r

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
+int _putchar(char c);
+
 /**
  * print_numbers - print 0-9
  * return: void
  */
-
-void print_numbers(void)
+void main(void)
 {
-	char c;
+	har c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
-	_putchar('\n');
+	putchar('\n');
+}
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
