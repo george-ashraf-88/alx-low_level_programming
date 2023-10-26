4b301bf488bbfb97f3c8d5249f1ccf2216d98924
george-ashraf-88 <georgeashraf88@icloud.com>
2023-10-25 20:25:29 -0700

    l

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * _strlen_recursion - main function
+  *
+  * @s: Function parameter
+  *
+  * Return: Length
+  */
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
