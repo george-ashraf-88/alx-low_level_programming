d9d1b55e945a0f82b41804f5b0c0feadfc2d5305
george-ashraf-88 <georgeashraf88@icloud.com>
2023-11-06 16:10:29 -0800

    L

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef PH_h
+#define PI_H
+
+#define pi 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-print the name of the file
+ * 
+ * return: 0
+ */
+
+int main (void)
+{
+	printf("%s\n",_FILE_);
+	return (0);
+}
