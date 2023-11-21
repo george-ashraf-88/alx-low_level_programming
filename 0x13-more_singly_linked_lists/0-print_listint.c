7dd0c43c3ca42b3c81a8b34f9fcc63e44ede436e
george-ashraf-88 <georgeashraf88@icloud.com>
2023-11-21 06:44:48 -0800

    l

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,7 +12,7 @@ size_t print_listint(const listint_t *h)
 	size_t node_count = 0;
 	const listint_t *current = h;
 
-	while (current != NULL)
+	while (current)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,16 +9,15 @@
  * @n: integer
  * @next: points to the next node
  *
- *
  * Description: singly linked list node structure
- * for alx
+ * for alx project
  */
 
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}	listint_t
+}	listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -32,4 +31,6 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
+
 #endif
+
