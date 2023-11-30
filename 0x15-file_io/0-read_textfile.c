d275386e41a061d8dd3c5af13fcc081f4052f534
george-ashraf-88 <georgeashraf88@icloud.com>
2023-11-30 15:33:18 -0800

    kk

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *file;
@@ -7,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
-		return(0);
+		return (0);
 	file = fopen(filename, "r");
 	if (file == NULL)
 		return (0);
