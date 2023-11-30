da239ca2b4bf2021fc0c3404cc26a888bf2b7350
george-ashraf-88 <georgeashraf88@icloud.com>
2023-11-30 15:10:49 -0800

    tt

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return(0);
+	fp *file = fopen(filename, "r");
+	if (file == NULL)
+		return (0);
+	char *buffer = (char *)malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+	ssize_t bytes_read = fread(buffer, sizeof(char), letters, file);
+	if (bytes_read <= 0)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+	buffer[bytes_read] = '\0';
+
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written <= 0 || (size_t)bytes_written != bytes_read)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+	fclose(file);
+	free(buffer);
+	return (bytes_written);
+}
